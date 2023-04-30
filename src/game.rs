6f7af17424b3940c84ae38995da1ec2e7027d441
memory-Ieak <dimitri.escaffrefaure@gmail.com>
2023-04-30 22:11:53 +0200

    add basic movement

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,29 @@
 pub mod board;
 
 pub fn run() {
-    let board = board::Board();
-    println!("{}", board);
+    let cmds = vec![
+        ("select", 1, 0)
+    ];
+    let mut board = board::Board::new();
+    let mut player: board::Color = board::Color::WHITE;
+    let mut i = 0;
+    /*'change_player: loop {
+        player = if player == board::Color::WHITE {
+            board::Color::BLACK
+        } else {
+            board::Color::WHITE
+        };
+        'play: loop {
+
+        }
+        i += 1;
+    }*/
+    for cmd in cmds {
+        board.print();
+        match cmd {
+            ("select", _, _) => board.select(cmd.1, cmd.2),
+            _ => continue,
+        }
+    }
+    board.print();
 }
--- src/game/board.rs
+++ src/game/board.rs
@@ -1,41 +1,198 @@
-enum Color {
-    White,
-    Black,
+use colored::Colorize;
+
+const BOARD_SIZE : usize = 8;
+
+#[derive(PartialEq, Eq)]
+pub enum Color {
+    BLACK,
+    WHITE,
+    NONE,
 }
 
-pub trait Piece {
-    fn move(&self);
+#[derive(PartialEq, Eq)]
+enum Overlay {
+    PATH,
+    KILL,
+    NONE,
 }
 
-pub struct King { color: Color, }
+#[derive(PartialEq, Eq)]
+enum Piece {
+    KING,
+    QUEEN,
+    BISHOP,
+    ROOK,
+    KNIGHT,
+    PAWN,
+    EMPTY,
+}
 
-impl King {
-    pub fn new(color: Color) -> Self {
-        King { 
-            color: color.into()
+struct Cell {
+    piece : Piece,
+    color : Color,
+    first_move : bool,
+    overlay : Overlay,
+}
+
+impl Cell {
+    fn king(color: Color) -> Self {
+        Cell { 
+            piece : Piece::KING, 
+            color : color, 
+            first_move : true,
+            overlay : Overlay::NONE,
         }
     }
-}
 
-impl Piece for King {
-    fn move(&self) {
-        println!("Default implementation for PrintsUpdate...");
+    fn queen(color: Color) -> Self {
+        Cell { 
+            piece : Piece::QUEEN, 
+            color : color, 
+            first_move : true,
+            overlay : Overlay::NONE,
+        }
+    }
+
+    fn bishop(color: Color) -> Self {
+        Cell { 
+            piece : Piece::BISHOP, 
+            color : color, 
+            first_move : true,
+            overlay : Overlay::NONE,
+        }
+    }
+
+    fn knight(color: Color) -> Self {
+        Cell { 
+            piece : Piece::KNIGHT, 
+            color : color, 
+            first_move : true,
+            overlay : Overlay::NONE,
+        }
+    }
+
+    fn rook(color: Color) -> Self {
+        Cell { 
+            piece : Piece::ROOK, 
+            color : color, 
+            first_move : true,
+            overlay : Overlay::NONE,
+        }
+    }
+
+    fn pawn(color: Color) -> Self {
+        Cell { 
+            piece : Piece::PAWN, 
+            color : color, 
+            first_move : true,
+            overlay : Overlay::NONE,
+        }
+    }
+
+    fn empty() -> Self {
+        Cell { 
+            piece : Piece::EMPTY, 
+            color : Color::NONE, 
+            first_move : true,
+            overlay : Overlay::NONE,
+        }
+    }
+
+    fn kill(&mut self) {
+        self.piece = Piece::EMPTY;
+        self.color = Color::NONE;
     }
 }
 
-
 pub struct Board {
-    pieces: [[&Piece; 8]; 8],
+    b : Vec<Vec<Cell>>,
 }
 
 impl Board {
-    fn init(&mut self) {
-        for i in &[(0,0), (0,7)] {
-            self.pieces[i.0][i.1] = Null;
+    pub fn new() -> Self {
+        Board { 
+            b: vec![vec![Cell::rook(Color::WHITE), Cell::knight(Color::WHITE), Cell::bishop(Color::WHITE), Cell::queen(Color::WHITE), Cell::king(Color::WHITE), Cell::bishop(Color::WHITE), Cell::knight(Color::WHITE), Cell::rook(Color::WHITE)],
+                    vec![Cell::pawn(Color::WHITE), Cell::pawn(Color::WHITE),   Cell::pawn(Color::WHITE),   Cell::pawn(Color::WHITE),  Cell::pawn(Color::WHITE), Cell::pawn(Color::WHITE),   Cell::pawn(Color::WHITE),   Cell::pawn(Color::WHITE)],
+                    vec![Cell::empty(),            Cell::empty(),              Cell::empty(),              Cell::empty(),             Cell::empty(),            Cell::empty(),              Cell::empty(),              Cell::empty()],
+                    vec![Cell::empty(),            Cell::empty(),              Cell::empty(),              Cell::empty(),             Cell::empty(),            Cell::empty(),              Cell::empty(),              Cell::empty()],
+
+                    vec![Cell::empty(),            Cell::empty(),              Cell::empty(),              Cell::empty(),             Cell::empty(),            Cell::empty(),              Cell::empty(),              Cell::empty()],
+                    vec![Cell::empty(),            Cell::empty(),              Cell::empty(),              Cell::empty(),             Cell::empty(),            Cell::empty(),              Cell::empty(),              Cell::empty()],
+                    vec![Cell::pawn(Color::BLACK), Cell::pawn(Color::BLACK),   Cell::pawn(Color::BLACK),   Cell::pawn(Color::BLACK),  Cell::pawn(Color::BLACK), Cell::pawn(Color::BLACK),   Cell::pawn(Color::BLACK),   Cell::pawn(Color::BLACK)],
+                    vec![Cell::rook(Color::BLACK), Cell::knight(Color::BLACK), Cell::bishop(Color::BLACK), Cell::queen(Color::BLACK), Cell::king(Color::BLACK), Cell::bishop(Color::BLACK), Cell::knight(Color::BLACK), Cell::rook(Color::BLACK)],],
         }
     }
 
-    fn print(&self) {
+    pub fn select(&mut self, x: usize, y: usize) {
+        match self.b[x][y].piece
+        {
+            Piece::PAWN => {
+                self.update_cell(x+1,y);
+                if self.b[x][y].first_move == true {
+                    self.update_cell(x+2,y);
+                }
+            }
+            _ => {}
+        }
+    }
+
+    pub fn move_piece(&mut self, x: usize, y: usize) {
         
+    }
+
+    fn check_cell(&self, x: usize, y: usize) -> bool
+    {
+        if x < 0 || y < 0 || x >= BOARD_SIZE || y >= BOARD_SIZE {
+            return false;
+        }
+        return true;
+    }
+
+    fn update_cell(&mut self, x: usize, y: usize) -> bool
+    {
+        if self.check_cell(x, y) {
+            if self.b[x][y].piece == Piece::EMPTY {
+                self.b[x][y].overlay = Overlay::PATH;
+                return false;
+            } else {
+                self.b[x][y].overlay = Overlay::KILL;
+                return true;
+            }
+        }
+        return true;
+    }
+
+    pub fn print(&self) {
+        let mut p: &str;
+        println!("----------");
+        println!(" abcdefgh");
+        for i in 0..BOARD_SIZE {
+            print!("{}", BOARD_SIZE-i);
+            for j in 0..BOARD_SIZE {
+                match self.b[i][j].piece {
+                    Piece::KING => p = "K",
+                    Piece::QUEEN => p = "Q",
+                    Piece::BISHOP => p = "B",
+                    Piece::ROOK => p = "R",
+                    Piece::KNIGHT => p = "k",
+                    Piece::PAWN => p = "P",
+                    _ => p = if self.b[i][j].overlay == Overlay::PATH { "." }
+                    else { " " },
+                }
+                let mut c : colored::ColoredString;
+                if self.b[i][j].color == Color::WHITE {
+                    c = p.white();
+                }
+                else {
+                    c = p.black();
+                }
+                if self.b[i][j].overlay == Overlay::KILL {
+                    c = c.strikethrough();
+                }
+                print!("{}", c)
+            }
+            println!("{}", BOARD_SIZE-i);
+        }
+        println!(" abcdefgh");
     }
 }
